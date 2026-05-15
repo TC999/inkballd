aa14f93cd2c61c994224b615a5b17eea5cd60af6
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2026-05-15 12:19:55 +0800

    重命名 && 合并同名文件

--- src/CBallManager.cpp
+++ src/CBallManager.cpp
@@ -76,3 +76,34 @@ CBallManager::CBallManager()
     log_state = -1;
     reinterpret_cast<Helpers::CLogBlock*>(log_buffer)->~CLogBlock();
 }
+
+extern "C" void* CBallManager_vftable; // Forward declaration of virtual table
+
+CBallManager::~CBallManager()
+{
+    CBall** ball_ptr;
+    int ball_count;
+    uint8_t log_buffer[16];
+    int flag;
+
+    *((uint32_t*)self + 0) = reinterpret_cast<uint32_t>(&CBallManager_vftable);
+    Helpers::CLogBlock::CLogBlock(reinterpret_cast<Helpers::CLogBlock*>(log_buffer), "CBallManager::~CBallManager", 0);
+    flag = 0;
+
+    ball_ptr = reinterpret_cast<CBall**>(reinterpret_cast<uint8_t*>(self) + 56);
+    ball_count = 6;
+
+    do
+    {
+        if (*ball_ptr)
+        {
+            CBall::Release(*ball_ptr);
+            *ball_ptr = nullptr;
+        }
+        ++ball_ptr;
+        --ball_count;
+    } while (ball_count);
+
+    flag = -1;
+    reinterpret_cast<Helpers::CLogBlock*>(log_buffer)->~CLogBlock();
+}
