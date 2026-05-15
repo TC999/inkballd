5a40ae8525a6676f090d748f60b850b0cb2b8ce3
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2026-05-15 11:56:16 +0800

    重命名 && 合并同名文件

--- src/AddBallToUpdateList.cpp
+++ src/AddBallToUpdateList.cpp
@@ -3,6 +3,22 @@
 #include <cstring>
 #include <cstdlib>
 #include <windows.h>
+
+extern "C" {
+    void __stdcall AddBallToUpdateList(void* ball)
+    {
+        uint8_t log_buffer[16]; // [esp+10h] [ebp-14h] BYREF
+        int cleanup_flag; // [esp+20h] [ebp-4h]
+
+        Helpers::CLogBlock::CLogBlock(reinterpret_cast<Helpers::CLogBlock*>(log_buffer), "AddBallToUpdateList", 0);
+        cleanup_flag = 0;
+        CGameBoard::AddBallToUpdateList(g_pCGameBoard, (CBall*)ball);
+        cleanup_flag = -1;
+        reinterpret_cast<Helpers::CLogBlock*>(log_buffer)->~CLogBlock();
+    }
+}
+
+
 void CGameBoard::AddBallToUpdateList(CGameBoard *self, CBall* a2)
 {
   int v3; // ecx
