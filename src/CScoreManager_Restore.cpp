8dbccb846c9c4afc3b2201db6fb68e77dc9b48bd
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2026-05-16 20:05:41 +0800

    补全 CScoreManager 伪代码

---
+++ src/CScoreManager_Restore.cpp
@@ -0,0 +1,15 @@
+//----- (0100D1CB) --------------------------------------------------------
+void __thiscall CScoreManager::Restore(CScoreManager *this)
+{
+  struct IDirectDrawSurface7 *DDrawSurface; // eax
+  _BYTE v3[16]; // [esp+10h] [ebp-14h] BYREF
+  int v4; // [esp+20h] [ebp-4h]
+
+  Helpers::CLogBlock::CLogBlock((Helpers::CLogBlock *)v3, "CScoreManager::Restore", 0);
+  v4 = 0;
+  DDrawSurface = CSurface::GetDDrawSurface(g_pScoreManagerSurface);
+  DDrawSurface->lpVtbl->Restore(DDrawSurface);
+  (*(void (__thiscall **)(CScoreManager *))(*(_DWORD *)this + 4))(this);
+  v4 = -1;
+  Helpers::CLogBlock::~CLogBlock((Helpers::CLogBlock *)v3);
+}
